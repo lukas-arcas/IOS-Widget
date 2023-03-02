5f2f87e7fe890455009bef68118d1951f7dba4fd
juli220901 <juli220901@gmail.com>
2023-03-02 10:43:43 +0100

    add Styling

--- Read MacStories.js
+++ Read MacStories.js
@@ -1,127 +1,154 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: red; icon-glyph: newspaper;
-// This script shows articles from MacStories. The script can be used either in the app, as a widget on your Home Screen or through Shortcuts. The behaviour of the script will vary slightly depending on where it's used.
-
+// icon-color: pink; icon-glyph: magic;
 const info = [
     ["The Tao of Mac", "31m ago", "displayplacer", "displayplacer is a cli utility to automate many of the"],
     ["heise Security", "46m ago", "Mehrere DoS-Lücken im Netzwerkanalytischen", "Wireshark 4.0.3 bringt einige Sicherheitsunwägbarkeiten"],
-    ["Test", "Test", "Test", "Test"],
-    ["Test", "Test", "Test", "Test"],
-    ["Test", "Test", "Test", "Test"]
+    ["heise Security", "1h ago", "Cyberkriminelle verschaffen sich Zugang zu", "Der Pay-TV Anbieter Sky bestätigt, dass sich einige"],
+    ["heise Security", "1h ago", "UEFI Secure Boot: MSI stellt Bequemlichkeit bei", "Der Computerhardware-Hersteller bestätigt über"],
+    ["iPhoneBlog.de", "1h ago", "Das iPad (10.Generation)", "Der folgende Artikel ist über 6000 Zeichen lang"]
 ];
 
-let widget = new ListWidget();
-widget.backgroundColor = Color.white()
 
-if(config.widgetFamily === "large"){
+function createWidget() {
 
-    createLWidget(widget)
+    // Neues Widget erstellen
+    let listwidget = new ListWidget();
 
-}
-else if(config.widgetFamily === "extraLarge"){
+    // Hintergrundfarbe festlegen
+    listwidget.backgroundColor = new Color("#ffffff");
 
-    createXlWidget(widget)
+    //Check der Widget-Größe
+    if(config.widgetFamily === "large"){
 
-}
-else{
+        createLWidget(listwidget)
 
-    createLWidget(widget)
-    //let test2 = widget.addText("else")
-    //test2.textColor = Color.black()
+    }
+    else if(config.widgetFamily === "extraLarge"){
 
-}
+        createXlWidget(listwidget)
 
-
-Script.setWidget(widget);
-widget.presentLarge();      //anzeige größe beim drauf klicken
-Script.complete();
+    }
 
 
-function createXlWidget(main){
-let main2 = widget.addStack()
-    main2.layoutHorizontally()
-
-    createLogo(main2)
-
-    let mainBlock = main2.addStack()
-        mainBlock.layoutVertically()
-        mainBlock.size = new Size(320,330)
-
-        createTextBlock(mainBlock)
-
-    main2.addSpacer(10)
-
-    let lineVertical = main2.addStack()
-        lineVertical.size = new Size(1,340)
-        lineVertical.backgroundColor = Color.black()
-
-    main2.addSpacer(10)
-
-
-    let mainBlock2 = main2.addStack()
-        mainBlock2.layoutVertically()
-        mainBlock2.size = new Size(320,330)
-
-        createTextBlock(mainBlock2)
-
+    // Erstellstes Widget zurückgeben
+    return listwidget;
 }
 
-function createLWidget(widget){
-let main = widget.addStack()
+function createLWidget(widget) {
+
+    let main = widget.addStack()
     main.layoutHorizontally()
 
     createLogo(main)
 
     let mainBlock = main.addStack()
-        mainBlock.layoutVertically()
-        mainBlock.size = new Size(270,330)
+    mainBlock.layoutVertically()
+    mainBlock.size = new Size(270, 330)
 
-        createTextBlock(mainBlock)
+    createTextBlock(mainBlock, 270)
 }
 
-function createLogo(main){
-let logoBlock = main.addStack()
-        logoBlock.setPadding(0,10,0,10)
-        logoBlock.layoutVertically()
-        logoBlock.size = new Size(65,330)
-        let icon = SFSymbol.named("tortoise")
-            let addIcon = logoBlock.addImage(icon.image)
+function createXlWidget(widget) {
+    let main2 = widget.addStack()
+    main2.layoutHorizontally()
+
+    createLogo(main2)
+
+    let mainBlock = main2.addStack()
+    mainBlock.layoutVertically()
+    mainBlock.size = new Size(310, 330)
+
+    createTextBlock(mainBlock, 310)
+
+    main2.addSpacer(10)
+
+    let lineVertical = main2.addStack()
+    lineVertical.size = new Size(1, 340)
+    lineVertical.backgroundColor = new Color("#E0EEEE")
+
+    main2.addSpacer(10)
+
+
+    let mainBlock2 = main2.addStack()
+    mainBlock2.layoutVertically()
+    mainBlock2.size = new Size(310, 330)
+
+    createTextBlock(mainBlock2, 310)
+
 }
 
-function createLine(mainBlock){
+function createLogo(main) {
+    let logoBlock = main.addStack()
+    logoBlock.setPadding(0, 10, 0, 10)
+    logoBlock.layoutVertically()
+    logoBlock.size = new Size(55, 330)
+    let icon = SFSymbol.named("tortoise")
+    let addIcon = logoBlock.addImage(icon.image)
+}
+
+function createLine(mainBlock) {
     let line = mainBlock.addStack()
-        line.size = new Size(270, 1)
-        line.backgroundColor = Color.gray() //!!!
+    line.size = new Size(270, 0.5)
+    line.backgroundColor = new Color("#E0EEEE") //!!!
 }
 
-function createTextBlock(mainBlock){
+function createTextBlock(mainBlock, sizeY) {
 
-max = 4;
+    if (info.length <= 5) {
+        max = info.length-1
+    }
+    else {
+        max = 4
+    }
 
- for(i=0; i <= max;i++){
- let textBlock = mainBlock.addStack()
+    for (i = 0; i <= max; i++) {
+
+        if (i > 0) {
+            mainBlock.addSpacer(5)
+        }
+
+        let textBlock = mainBlock.addStack()
         textBlock.layoutVertically()
-        textBlock.size = new Size(270, 65.2)
+        textBlock.size = new Size(sizeY, 53)
 
-       let sourceTime = textBlock.addText(info[i][0] + " • " + info[i][1]);
-       //heading.centerAlignText();
-           sourceTime.font = Font.lightSystemFont(15);
-           sourceTime.textColor = new Color("#D3D3D3");
+        // Quelle und Zeitdifferenz einfügen
+        let sourceTimeBlock = textBlock.addStack()
+            sourceTimeBlock.layoutHorizontally()
+            let source = sourceTimeBlock.addText(info[i][0] + " • ")
+                source.font = Font.systemFont(12);
+                source.textColor = new Color("#838B8B");
+            let time = sourceTimeBlock.addText(info[i][1])
+                time.font = Font.lightSystemFont(12);
+                time.textColor = new Color("#838B8B");
+
 
         // Titel einfügen
-       let title = textBlock.addText(info[i][2]);
-           title.font = Font.systemFont(20);
-           title.textColor = new Color("#000000");
+        let title = textBlock.addText(info[i][2]);
+        title.font = Font.systemFont(16);
+        title.textColor = new Color("#000000");
 
-       // Inhalt einfügen
-       let text = textBlock.addText(info[i][3]);
-           text.font = Font.lightSystemFont(15);
-           text.textColor = new Color("#000000");
+        // Inhalt einfügen
+        let text = textBlock.addText(info[i][3]);
+        text.font = Font.lightSystemFont(15);
+        text.textColor = new Color("#000000");
 
-       if(i < max){
-        createLine(mainBlock)
-       }
- }
+        // Trennstrich Zwischen den einzelnen Einträgen
+        if (i < max) {
+            mainBlock.addSpacer(5)
+            createLine(mainBlock)
+        }
+    }
+}
 
- }
+let widget = await createWidget();
+
+// Überprüfen wo das Scribt ausgeführt wird
+if (config.runsInWidget) {
+    // Im Widget ausgeführt, auf Homescreen anzeigen
+    Script.setWidget(widget);
+} else {
+    // In der App ausgeführt
+    widget.presentLarge();
+}
+Script.complete();
